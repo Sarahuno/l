9293ba50293fc16fe86323fadd84f3dea2ab7aab
Sarahuno <122272550+Sarahuno@users.noreply.github.com>
2024-05-28 17:37:18 +0200

    Update rickroll.php

--- rickroll.php
+++ rickroll.php
@@ -1,1 +1,48 @@
 
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Redirecting...</title>
+
+    <meta http-equiv="refresh" content="0.8;url=https://www.youtube.com/watch?si=-P26CfOYPaOK4tIM&v=dQw4w9WgXcQ&feature=youtu.be">
+
+    <style>
+
+        body, html {
+
+            margin: 0;
+
+            padding: 0;
+
+            width: 100%;
+
+            height: 100%;
+
+            overflow: hidden;
+
+        }
+
+        img {
+
+            width: 100%;
+
+            height: 100%;
+
+            object-fit: contain;
+
+        }
+
+    </style>
+
+</head>
+
+<body style="margin: 0; padding: 0;">
+
+    <img src="https://c.tenor.com/u_sEs0NNEIYAAAAC/tenor.gif" alt="Loading...">
+
+</body>
+
+</html>
